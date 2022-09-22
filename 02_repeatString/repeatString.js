7f4cdafc6e5233c3db9c6038676e8ee3c54b3872
RHammond <Rhammond.1988@gmail.com>
2022-09-22 14:50:48 -0700

    complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+let echo = ''
+const repeatString = function(string, num) {
+    if (num >=0) {
+        echo = '';
+        for (let i = num ; i > 0; i--) {
+            echo = echo+string;
+        }
+        return echo;
+    } else {
+        echo = 'ERROR';
+        return echo;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;

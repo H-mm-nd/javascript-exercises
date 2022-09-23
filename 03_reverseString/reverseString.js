37602d7f28153d157c96d1dfbed4fd6e26e0fb9f
RHammond <Rhammond.1988@gmail.com>
2022-09-22 18:53:15 -0700

    complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split('');
+    stringReversed = array.reverse().join("");
+    return stringReversed;
 };
 
 // Do not edit below this line

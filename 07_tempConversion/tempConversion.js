6413892327dec377a4fe000176081b4855f41b2b
RHammond <Rhammond.1988@gmail.com>
2022-09-25 11:40:32 -0700

    complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  // this includes all the various math steps
+  //return ((Math.round(((fahrenheit-32)*5/9)*10))/10);
+  //this one is simplified; 4 sig figs chosen for simplification
+  return (Math.round((fahrenheit-32)*5.555)/10);
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  return (Math.round(((celsius*9/5)+32)*10)/10);
 };
 
 // Do not edit below this line

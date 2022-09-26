2a4d1c818aae568be03c4f1b249cbd8b3d6127c1
RHammond <Rhammond.1988@gmail.com>
2022-09-25 18:57:01 -0700

    complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+  return (number1+number2);
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+  return (number1-number2);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  array.forEach((item) => {
+    sum += item;
+  });
+  return (sum);
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  // this worked but i incorrectly thought the input was a series of argument rather than a array
+  //  let product = 1
+  //  for (let i=0; i < arguments.length; i++) {
+  //    product *= arguments[i];
+  //  }
+  //  return (product);
 
+  let product = 1;
+  array.forEach((item) => {
+    product *= item;
+  });
+  return (product);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	let answer = 1;
+  for (let i=1; i <= exponent; i++) {
+    answer *= base;
+  }
+  return (answer);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let answer = 1;
+  for (let i=1; i <= number; i++) {
+    answer *= i;
+  }
+  return (answer);
 };
 
 // Do not edit below this line

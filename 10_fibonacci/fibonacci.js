4c5a2aea9f2f8a681ab97ac0e46f530031b3e154
RHammond <Rhammond.1988@gmail.com>
2022-10-01 13:52:02 -0700

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let member = parseInt(number);
+    if (member === 0) {return(0)}
+    if (member < 0) {return('OOPS')}
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < member; i++) {
+        const temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return(b);
 };
 
+//function fibonacci (number){
+//   let member = parseInt(number);
+//    if (member === 0) {return(0)}
+//    if (member < 0) {return('OOPS')}
+//    let a = 0;
+//    let b = 1;
+//    for (let i = 1; i < member; i++) {
+//        const temp = b;
+//        b = a + b;
+//        a = temp;
+//        console.log(b);
+//    }
+//   return(b);
+//}
 // Do not edit below this line
 module.exports = fibonacci;
